d97a65bf3bc084fa38b7d06cde30fada19aa7224
nhquang192 <nhquang192@gmail.com>
2022-07-14 10:47:03 +0700

    Example1: Add google font to project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <router-view />
-  <p class="h-10 w-20 bg-slate-600 text-3xl text-blue-600">Hell O</p>
+  <p class="h-10 text-3xl font-semibold text-blue-600">Hell O 12345678</p>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inconsolata", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+@import url("https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700&display=swap");
 </style>

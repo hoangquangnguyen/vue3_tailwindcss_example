f58cb5809d117bf1a9ef33a2a66bc6c53517fd4c
nhquang192 <nhquang192@gmail.com>
2022-07-14 10:25:36 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
+  <p class="h-10 w-20 bg-slate-600 text-3xl text-blue-600">Hell O</p>
 </template>
 
 <style>
@@ -13,18 +10,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html","./src/**/*.{vue,ts,js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
